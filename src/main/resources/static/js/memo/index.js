22d3c2464b0d6cd33841f3c7ee406a934051e4e7
yuichi_develop <nepinepi984@gmail.com>
2019-11-18 21:56:49 +0900

    候補入力の実装（Front)

--- src/main/resources/static/js/memo/index.js
+++ src/main/resources/static/js/memo/index.js
@@ -26,6 +26,13 @@ $(function() {
 			ajaxDelete(url, _mmid, _name);
 		}
 	});
+	$(document).on('click', ".tag, .blue, .tag_many", function(){
+		var _mmnm = $(this).data('mmnm');
+		var _mmsb = $(this).data('mmsb');
+		console.log(_mmnm);
+		console.log(_mmsb);
+		ajaxPost(_mmnm, url, _mmsb);
+	});
 });
 
 // メモ画面共通のクリアメソッド
@@ -46,12 +53,12 @@ function checkInput(data) {
 }
 // 「食費」 要素追加メソッド
 function appendFood(data){
-//	card-block
+// card-block
 	$('#card-food').append('<div id=card-food-' + data["mmid"] + '><p class="card-footer"><span class="left">' + data["mmnm"] + '</span> <span class="right"><button id="btn-food" name="card-food" class="btn btn-outline-info btn-sm waves-effect waves-light" data-mmid=' + data['mmid'] + '>X</button></span></p></div>')
 }
-//「日用品」 要素追加メソッド
+// 「日用品」 要素追加メソッド
 function appendNitiyou(data){
-//	card-block
+// card-block
 	$('#card-nitiyou').append('<div id=card-nitiyou-' + data["mmid"] + '><p class="card-footer"><span class="left">' + data["mmnm"] + '</span> <span class="right"><button id="btn-food" name="card-nitiyou" class="btn btn-outline-info btn-sm waves-effect waves-light" data-mmid=' + data['mmid'] +  '>X</button></span></p></div>')
 }
 // メモを登録するAPIをたたく
